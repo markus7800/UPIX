fa484c00786c605b62e033cd8bd4ceff941edc52
Markus Böck <markus.boeck7800@hotmail.com>
2025-01-23 16:09:24 +0100

    good working prototype

--- dccxjax/tracer.py
+++ dccxjax/tracer.py
@@ -7,6 +7,7 @@ import jax
 from typing import List, Optional, Dict, Tuple, Any, Callable
 from abc import ABC, abstractmethod
 from jax.tree_util import tree_flatten, tree_unflatten
+import jax.numpy as jnp
 
 
 def primitive_name(primitive: jax_core.Primitive, params):
@@ -32,7 +33,7 @@ class SOp(SExpr):
         args = [arg.eval(X) for arg in self.args]
         # print("eval", primitive_name(self.primitive, self.params), "with", args, self.args, self.params)
         # out = self.primitive.impl(*args, **self.params)
-        out = self.primitive.bind(*args, **self.params) # bind insetad of impl is important here to be able to jit-compile it
+        out = self.primitive.bind(*args, **self.params) # bind instead of impl is important here to be able to jit-compile it later
         # pjit always returns []
         if self.primitive.multiple_results:
             if self.primitive is jax_pjit.pjit_p and self.params["out_layouts"] == (None,):
@@ -44,6 +45,24 @@ class SOp(SExpr):
         else:
             # print("out1 =", out)
             return out
+    def __eq__(self, value: object) -> bool:
+        if isinstance(value, SOp):
+            if self.primitive != value.primitive:
+                return False
+            if self.primitive is jax_pjit.pjit_p:
+                if self.params["jaxpr"] != value.params["jaxpr"]:
+                    return False
+            if len(self.args) != len(value.args):
+                return False
+            for (a1, a2) in zip(self.args, value.args):
+                if a1 != a2:
+                    return False
+            return True
+        return False
+    
+    def __hash__(self) -> int:
+        return hash((primitive_name(self.primitive, self.params), tuple(self.args)))
+        
     
 # if a functions is jitted then the call to pjit_p.bind is in _python_pjit_helper
 # in this helper function the input and output are flattened and unflattend according to in_tree and out_tree
@@ -58,6 +77,18 @@ class SConstant(SExpr):
         return f"Constant({self.constant})"
     def eval(self, X: dict[str, jax.Array]):
         return self.constant
+    def __eq__(self, value: object) -> bool:
+        if isinstance(value, SConstant):
+            if isinstance(self.constant, jax.Array) and isinstance(value.constant, jax.Array):
+                return bool(jnp.all(self.constant == value.constant))
+            return self.constant == value.constant
+        return False
+    def __hash__(self) -> int:
+        try:
+            return hash(self.constant)
+        except TypeError:
+            return 0
+    
 
 class SVar(SExpr):
     def __init__(self, name: str) -> None:
@@ -66,23 +97,17 @@ class SVar(SExpr):
         return self.name
     def eval(self, X: dict[str, jax.Array]) -> jax.Array:
         return X[self.name]
-
-
-def find_constant(sexpr: SExpr, constant):
-    if isinstance(sexpr, SConstant):
-        if sexpr.constant is constant: # id(sexpr.constant) == id(constant)
-            return True, sexpr
-    elif isinstance(sexpr, SOp):
-        for arg in sexpr.args:
-            found, constant = find_constant(arg, constant)
-            if found:
-                return True, constant
-            
-    return False, None
+    def __eq__(self, value: object) -> bool:
+        if isinstance(value, SVar):
+            return self.name == value.name
+        return False
+    def __hash__(self) -> int:
+        return hash(self.name)
 
 def replace_constant_with_svars(input_object_ids_to_name: Dict[int, str], sexpr: SExpr) -> SExpr:
     if isinstance(sexpr, SConstant):
-        if id(sexpr.constant) in input_object_ids_to_name:
+        # only replace jax.Array
+        if isinstance(sexpr.constant, jax.Array) and id(sexpr.constant) in input_object_ids_to_name:
             return SVar(input_object_ids_to_name[id(sexpr.constant)])
         else:
             return sexpr
@@ -93,15 +118,31 @@ def replace_constant_with_svars(input_object_ids_to_name: Dict[int, str], sexpr:
         assert isinstance(sexpr, SVar)
         return sexpr
             
+
+class BranchingDecisions:
+    def __init__(self, object_id_to_sym_name: Dict[int, str] = {}) -> None:
+        self.boolean_decisions: Dict[SExpr, bool] = {}
+        self.index_decisions: Dict[SExpr, int] = {}
+        self.object_id_to_sym_name = object_id_to_sym_name
+
+
 # based on JVPTrace / JVPTracer
 
 class BranchingTracer(jax_core.Tracer):
     
     def __init__(self, trace: jax_core.Trace, val, sexpr: Optional[SExpr] = None):
+        assert isinstance(trace, BranchingTrace)
         print("Init BranchingTracer with", val, type(val), sexpr)
         self._trace = trace
         self.val = val
-        self.sexpr: SExpr = sexpr if sexpr is not None else SConstant(val)
+        if sexpr is not None:
+            self.sexpr = sexpr
+        else:
+            object_id_to_sym_name = trace.branching_decisions.object_id_to_sym_name
+            if isinstance(val, jax.Array) and id(val) in object_id_to_sym_name:
+                self.sexpr = SVar(object_id_to_sym_name[id(val)])
+            else:
+                self.sexpr = SConstant(val)
 
     @property
     def aval(self):
@@ -110,11 +151,21 @@ class BranchingTracer(jax_core.Tracer):
     # def to_concrete_value(self):
     #     return jax_core.to_concrete_value(self.val)
 
-    # def __bool__(self):
-    #     return True
-
-    # def __index__(self):
-    #     assert False
+    def __bool__(self):
+        assert isinstance(self._trace, BranchingTrace)
+        boolean_decisions = self._trace.branching_decisions.boolean_decisions
+        if self.sexpr not in boolean_decisions:
+            b = bool(jax_core.to_concrete_value(self.val))
+            boolean_decisions[self.sexpr] = b
+        return boolean_decisions[self.sexpr]
+        
+    def __index__(self):
+        assert isinstance(self._trace, BranchingTrace)
+        index_decisions = self._trace.branching_decisions.index_decisions
+        if self.sexpr not in index_decisions:
+            b = int(jax_core.to_concrete_value(self.val)) # type: ignore
+            index_decisions[self.sexpr] = b
+        return index_decisions[self.sexpr]
 
     def full_lower(self):
         return jax_core.full_lower(self.val)
@@ -129,22 +180,22 @@ class BranchingTracer(jax_core.Tracer):
 #  = _defer_to_unrecognized_arg(op_symbol, ufunc.op) # Ensure that other array types have the chance to override arithmetic.
 #  = @export @partial(jit, inline=True) def op(x: ArrayLike, /) -> Array: ...
 
-def maybe_branching_tracer(trace: jax_core.Trace, val, sexpr: Optional[SExpr] = None):
+def maybe_branching_tracer(trace: "BranchingTrace", val, sexpr: Optional[SExpr] = None):
     try:
         jax_core.get_aval(val)
         return BranchingTracer(trace, val, sexpr)
     except TypeError:
         return val
     
-    # try jax_core.get_aval(val)
-    if isinstance(val, jax.Array):
-        return BranchingTracer(trace, val, sexpr)
-    else:
-        return val
+    # if isinstance(val, jax.Array):
+    #     return BranchingTracer(trace, val, sexpr)
+    # else:
+    #     return val
 
 class BranchingTrace(jax_core.Trace):
-    def __init__(self, parent_trace) -> None:
+    def __init__(self, parent_trace, branching_decisions: BranchingDecisions) -> None:
         self.parent_trace = parent_trace
+        self.branching_decisions = branching_decisions
 
     def process_primitive(self, primitive: jax_core.Primitive, tracers, params):
         print("process_primitive", primitive_name(primitive, params), tracers)
@@ -163,11 +214,11 @@ class BranchingTrace(jax_core.Trace):
         return out_tracer
 
 
-def detect_branching(f: Callable, sexprs: List[Tuple[SExpr,Any]]):
+def detect_branching(f: Callable, branching_decisions: BranchingDecisions):
     def _f(*args):
         with jax_core.take_current_trace() as parent_trace:
             print("parent_trace:", parent_trace)
-            trace = BranchingTrace(parent_trace)
+            trace = BranchingTrace(parent_trace, branching_decisions)
             with jax_core.set_current_trace(trace):
                 in_flat, in_tree = tree_flatten(args)
                 in_flat = map(lambda x: maybe_branching_tracer(trace, x), in_flat)
@@ -176,9 +227,6 @@ def detect_branching(f: Callable, sexprs: List[Tuple[SExpr,Any]]):
                 in_tracers = tree_unflatten(in_tree, in_flat)
                 out = f(*in_tracers)
                 out_flat, out_tree = tree_flatten(out)
-                for x in out_flat:
-                    if isinstance(x, BranchingTracer):
-                        sexprs.append((x.sexpr, x.val))
                 out_flat = map(lambda x: x.val if isinstance(x, BranchingTracer) else x, out_flat)
                 return tree_unflatten(out_tree, out_flat)
     return _f
--- test_detect_branching.py
+++ test_detect_branching.py
@@ -7,7 +7,7 @@ from typing import List, Tuple, Any
 
 
 def test_1(x):
-    return jax.lax.sin(x)
+    return bool(jax.lax.sin(x).sum() > 0)
 f = test_1
 args = [2.]
 
@@ -18,7 +18,7 @@ def test_2_inner(d: dict):
 def test_2(d: dict):
     print("Call test_2 with", d)
     x = test_2_inner(d)
-    return jax.lax.sin(x)
+    return bool(jax.lax.sin(x).sum() > 0)
 f = test_2
 args = [{"x": 2.}]
 
@@ -29,19 +29,17 @@ args = [{"x": A}]
 print("Run f:", f(*args))
 # exit()
 
-sexprs: List[Tuple[SExpr, Any]] = []
-out = detect_branching(f, sexprs)(*args)
+decisions = BranchingDecisions()
+out = detect_branching(f, decisions)(*args)
 print("returns", out)
-print("sexprs =", sexprs)
-for (sexpr, val) in sexprs:
+for (sexpr, val) in decisions.boolean_decisions.items():
     print("sexpr =", sexpr)
     print(val, "vs", sexpr.eval({}))
     jaxpr = jax.make_jaxpr(sexpr.eval)({})
     print(jaxpr)
     print("consts =", jaxpr.consts)
     jaxpr_f = jax_core.jaxpr_as_fun(jaxpr)
-    print(jaxpr_f())
-    print(find_constant(sexpr, A))
+    print("jaxpr_f() =", jaxpr_f())
     input_object_ids_to_name = {id(A): "A"}
     new_sexpr = replace_constant_with_svars(input_object_ids_to_name, sexpr)
     print("new_sexpr =", new_sexpr)
@@ -49,10 +47,27 @@ for (sexpr, val) in sexprs:
     print(new_jaxpr)
     print("consts =", new_jaxpr.consts)
     new_jaxpr_f = jax_core.jaxpr_as_fun(new_jaxpr)
-    print(new_jaxpr_f(A + 0.1))
+    print("new_jaxpr_f =", new_jaxpr_f(A + 0.1))
 
     new_jitted_f = jax.jit(new_sexpr.eval)
-    print(new_jitted_f({"A": A}))
+    print("new_jitted_f =", new_jitted_f({"A": A + 0.1}))
+
+
+input_object_ids_to_name = {id(A): "A"}
+decisions = BranchingDecisions(input_object_ids_to_name)
+out = detect_branching(f, decisions)(*args)
+print("returns", out)
+for (sexpr, val) in decisions.boolean_decisions.items():
+    print("sexpr =", sexpr)
+    print(val, "vs", sexpr.eval({"A": A}))
+    jaxpr = jax.make_jaxpr(sexpr.eval)({"A": A})
+    print(jaxpr)
+    print("consts =", jaxpr.consts)
+    jaxpr_f = jax_core.jaxpr_as_fun(jaxpr)
+    print("jaxpr_f =", jaxpr_f(A + 0.1))
+    jitted_f = jax.jit(sexpr.eval)
+    print("jitted_f =", jitted_f({"A": A + 0.1}))
+    
 
 
 
