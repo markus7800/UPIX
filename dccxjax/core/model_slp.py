somewhat working example, but smcdcc gives wrong results

dccxjax/core/model_slp.py
@@ -203,7 +203,7 @@ class SLP:
 
         self._gen_likelihood_weight = _make_slp_gen_likelihood_weight(self, model, branching_decisions)
 
-        self._unconstrained_log_prior_likeli_pathcond= _make_slp_unconstrained_log_prior_likeli_pathcond(self, model, branching_decisions)
+        self._unconstrained_log_prior_likeli_pathcond = _make_slp_unconstrained_log_prior_likeli_pathcond(self, model, branching_decisions)
 
         # this is not jitted
         # self._transform_unconstrained_to_support = _make_slp_transform_unconstrained_to_support(self, model, branching_decisions)
@@ -258,10 +258,10 @@ class SLP:
         log_prior, log_likelihood, path_condition = self._log_prior_likeli_pathcond(X, data_annealing)
         return jax.lax.select(path_condition, log_likelihood, jax.lax.full_like(log_likelihood, -jnp.inf))
     
-    def log_prob(self, X: Trace, data_annealing: AnnealingMask = dict()) -> FloatArray:
+    def log_prob(self, X: Trace, temperature: FloatArray = jnp.array(1.,float), data_annealing: AnnealingMask = dict()) -> FloatArray:
         assert self.decision_representative.keys() == X.keys()
         log_prior, log_likelihood, path_condition = self._log_prior_likeli_pathcond(X, data_annealing)
-        lp = log_prior + log_likelihood
+        lp = log_prior + temperature * log_likelihood
 
         # However, when transformed with vmap() to operate over a batch of predicates, cond is converted to select().
         # return jax.lax.cond(path_indicator(X), model_logprob, lambda _: -jnp.inf, X)
@@ -277,10 +277,10 @@ class SLP:
         log_prior, log_likelihood, path_condition, X_constrained = self._unconstrained_log_prior_likeli_pathcond(X_unconstrained, data_annealing)
         return jax.lax.select(path_condition, log_likelihood, jax.lax.full_like(log_likelihood, -jnp.inf)), X_constrained
     
-    def unconstrained_log_prob(self, X_unconstrained: Trace, data_annealing: AnnealingMask = dict()) -> Tuple[FloatArray, Trace]:
+    def unconstrained_log_prob(self, X_unconstrained: Trace, temperature: FloatArray = jnp.array(1.,float), data_annealing: AnnealingMask = dict()) -> Tuple[FloatArray, Trace]:
         assert self.decision_representative.keys() == X_unconstrained.keys()
         log_prior, log_likelihood, path_condition, X_constrained = self._unconstrained_log_prior_likeli_pathcond(X_unconstrained, data_annealing)
-        lp = log_prior + log_likelihood
+        lp = log_prior + temperature * log_likelihood
         return jax.lax.select(path_condition, lp, jax.lax.full_like(lp, -jnp.inf)), X_constrained
     
     def transform_to_constrained(self, X_unconstrained: Trace) -> Trace:
@@ -296,6 +296,13 @@ class SLP:
                 self.model()
                 return ctx.X_unconstrained
         return retrace_branching(_transform_to_unconstrained, self.branching_decisions)(X)[0]
+        
+    def generate(self, rng_key: PRNGKey, Y: Trace = dict()):
+        def _generate(rng_key: PRNGKey):
+            with GenerateCtx(rng_key, Y) as ctx:
+                self.model()
+                return ctx.X, ctx.log_likelihood + ctx.log_prior
+        return retrace_branching(_generate, self.branching_decisions)(rng_key)[0]
         
     def get_is_discrete_map(self):
         if self.is_discrete_map is None:

dccxjax/infer/smc.py
@@ -126,7 +126,7 @@ def tempering_schedule_from_array(arr: FloatArray):
     return TemperetureSchedule(arr, arr.shape[0])
 
 def tempering_schedule_from_sigmoid(linspace: FloatArray):
-    arr = sigmoid(linspace)
+    arr = sigmoid(linspace).at[-1].set(1.)
     return tempering_schedule_from_array(arr)
 
 class SMCState(NamedTuple):
@@ -288,10 +288,8 @@ class SMC:
     def run(self, rng_key: PRNGKey, particles: StackedTrace, log_prob: Optional[FloatArray] = None):
         # no tempering / annealing weights, we require that this is proper weighting for input particles
         if log_prob is None:
-            first_data_annealing = self.data_annealing_schedule.prior_mask()
-            log_prior, _, path_condition = jax.vmap(self.slp._log_prior_likeli_pathcond, in_axes=(0,None))(particles.data, first_data_annealing)
-            log_particle_weights = jax.lax.select(path_condition, log_prior, jax.lax.zeros_like_array(log_prior))
-            log_prob = log_prior
+            prior_data_annealing = self.data_annealing_schedule.prior_mask()
+            log_prob = jax.vmap(self.slp.log_prob, in_axes=(0,None,None))(particles.data, jnp.array(0.,float), prior_data_annealing)
 
         # print(log_particle_weights)
         log_particle_weights = jax.lax.zeros_like_array(log_prob)
@@ -318,14 +316,13 @@ class SMC:
 
 
 def get_log_Z_ESS(log_weights: FloatArray):
-    log_Z = jax.scipy.special.logsumexp(log_weights) - jnp.log(log_weights.size)
-    ESS = jnp.exp(jax.scipy.special.logsumexp(log_weights)*2 - jax.scipy.special.logsumexp(log_weights*2))
+    log_Z = jax.scipy.special.logsumexp(log_weights) - jax.lax.log(log_weights.size)
+    ESS = jax.lax.exp(jax.scipy.special.logsumexp(log_weights)*2 - jax.scipy.special.logsumexp(log_weights*2))
     return log_Z, ESS
 
 def get_Z_ESS(log_weights: FloatArray):
-    Z = jnp.exp(jax.scipy.special.logsumexp(log_weights)) / log_weights.size
-    ESS = jnp.exp(jax.scipy.special.logsumexp(log_weights)*2 - jax.scipy.special.logsumexp(log_weights*2))
-    return Z, ESS
+    log_Z, ESS = get_log_Z_ESS(log_weights)
+    return jax.lax.exp(log_Z), ESS
 
 def normalise_log_weights(log_weights: FloatArray):
     return log_weights - jax.scipy.special.logsumexp(log_weights)

evaluation/gp/gp.py
@@ -154,8 +154,18 @@ class SMCDCCConfig(SMCDCC[T]):
         active_slps.sort(key=self.model.slp_sort_key)
         print(f"{len(active_slps)=}")
 
+    def produce_samples_from_prior(self, slp: SLP, rng_key: PRNGKey) -> Tuple[StackedTrace, Optional[FloatArray]]:
+        Y: Trace = {addr: value  for addr,value in slp.decision_representative.items() if SuffixSelector("node_type").contains(addr)}    
+        particles, _ = jax.vmap(slp.generate, in_axes=(0,None))(jax.random.split(rng_key, self.smc_n_particles), Y)
+        return StackedTrace(particles, self.smc_n_particles), None
+    
     def get_SMC_rejuvination_kernel(self, slp: SLP) -> MCMCRegime:
-        regime = MCMCStep(PredicateSelector(lambda addr: not addr.endswith("node_type")), HMC(10, 0.02))
+        # regime = MCMCStep(PredicateSelector(lambda addr: not addr.endswith("node_type")), HMC(10, 0.02))
+        selector = PredicateSelector(lambda addr: not addr.endswith("node_type"))
+        regime = MCMCSteps(
+            MCMCStep(selector, RW(lambda _: dist.Normal(0.,1.), elementwise=True)),
+            MCMCStep(selector, HMC(10, 0.02))
+        )
         # pprint_mcmc_regime(regime, slp)
         return regime
     
@@ -164,11 +174,11 @@ class SMCDCCConfig(SMCDCC[T]):
     #     return data_annealing_schedule_from_range({"obs": range(step,len(ys),step)})
     
     def get_SMC_tempering_schedule(self, slp: SLP) -> Optional[TemperetureSchedule]:
-        return tempering_schedule_from_sigmoid(jnp.linspace(-15,15,10))
+        schedule = tempering_schedule_from_sigmoid(jnp.linspace(-5,5,100))
+        return schedule
 
 smc_dcc_obj = SMCDCCConfig(m, verbose=2,
     smc_n_particles=100,
-    smc_prior_mcmc_n_steps=1,
     smc_collect_inference_info=True,
 )
 