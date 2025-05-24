ef6a239df9a3676abcd2740b504426c0229a7dea
markus7800 <markus.boeck7800@hotmail.com>
2025-05-24 10:52:04 +0200

    test device memory + pmap

---
+++ test_device_memory.py
@@ -0,0 +1,38 @@
+import jax
+print(jax.devices('cpu'))
+print(jax.devices('gpu'))
+
+cpu = jax.devices('cpu')[0]
+gpu = jax.devices('gpu')[0]
+
+rng_key = jax.random.PRNGKey(0)
+arrs = []
+for i in range(100):
+    print(i)
+    rng_key, sample_key = jax.random.split(rng_key)
+    arr = jax.random.normal(sample_key, (1000*1000, 25))
+    arr.block_until_ready()
+    arrs.append(arr) # should be 100MB
+    if i % 10 == 0:
+        for j in range(i):
+            # arrs[j] = jax.device_put(arrs[j], device=cpu)
+            arrs[j] = jax.device_get(arrs[j]) # transfer to host
+
+
+def f(rng_key) -> jax.Array:
+    print(rng_key)
+    return jax.random.normal(rng_key, (1000*1000, 25))
+
+f_jitted = jax.jit(f)
+
+res_1 = f_jitted(jax.device_put(rng_key, device=cpu))
+print(res_1.device)
+res_2 = f_jitted(jax.device_put(rng_key, device=gpu))
+print(res_2.device)
+
+f_jitted_2 = jax.jit(f, device=gpu)
+
+res_1 = f_jitted_2(jax.device_put(rng_key, device=cpu))
+print(res_1.device)
+res_2 = f_jitted_2(jax.device_put(rng_key, device=gpu))
+print(res_2.device)
---
+++ test_parallelism.py
@@ -0,0 +1,52 @@
+
+from time import time
+
+import os
+
+os.environ["XLA_FLAGS"] = " ".join(
+    ["--xla_force_host_platform_device_count=8"]
+)
+import jax
+jax.config.update("jax_platform_name", "cpu")
+
+cpus = jax.devices('cpu')
+print(cpus)
+
+N = 100_000_000
+@jax.jit
+def f(rng_key):
+    def step(carry, key):
+        return carry + jax.random.normal(key), None
+    return jax.lax.scan(step, 0., jax.random.split(rng_key,N))[0]
+
+
+# t0 = time()
+# res = f(jax.random.PRNGKey(0))
+# res.block_until_ready()
+# t1 = time()
+# print(f"Finished in {t1-t0:.3f}s")
+
+t0 = time()
+res = jax.vmap(f)(jax.random.split(jax.random.PRNGKey(0), (8,)))
+res.block_until_ready()
+print(res)
+t1 = time()
+print(f"Finished in {t1-t0:.3f}s")
+
+t0 = time()
+res = jax.pmap(f)(jax.random.split(jax.random.PRNGKey(0), (8,)))
+res.block_until_ready()
+print(res)
+t1 = time()
+print(f"Finished in {t1-t0:.3f}s")
+
+t0 = time()
+res = []
+for i, key in enumerate(jax.random.split(jax.random.PRNGKey(0), (8,))):
+    print(i)
+    r = jax.jit(f, device=cpus[i])(jax.device_put(key, cpus[i]))
+    res.append(r)
+print(res)
+t1 = time()
+print(f"Finished in {t1-t0:.3f}s")
+
