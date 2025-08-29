985a1ac6600664a8bcffd4278d0e1c04bef3ee95
Markus Böck <markus.boeck7800@hotmail.com>
2025-08-29 09:49:17 +0200

    simplify and fix new ground truth

--- evaluation/pedestrian/ground_truth.py
+++ evaluation/pedestrian/ground_truth.py
@@ -7,6 +7,7 @@ parser.add_argument("batch_size", type=int)
 parser.add_argument("num_batches", type=int)
 parser.add_argument("n_iter", type=int)
 parser.add_argument("--show_plots", action="store_true")
+parser.add_argument("--no_save", action="store_true")
 parser.add_argument("--old", action="store_true")
 args = parser.parse_args()
 setup_devices_from_args(args)
@@ -45,12 +46,6 @@ def pedestrian(rng_key: jax.Array):
     return {"start": start, "lp": likelihood}
 
 
-@jax.jit
-def cdf(x, qs, weights: jax.Array):
-    def _cdf(q):
-        return jnp.where(x < q, weights, jax.numpy.zeros_like(weights)).sum()
-    return jax.lax.map(_cdf, qs)
-
 from dccxjax.parallelisation import *
 from dccxjax.progress_bar import ProgressbarManager
 from dccxjax.parallelisation import VectorisationType
@@ -72,7 +67,7 @@ def cdf_cruncher_old(qs, n_iter, batch_size):
         c += cdf(x, qs, weights) * 1/n_iter
     return c
 
-def cdf_cruncher(qs, batch_size, num_batches, n_iter, vectorisation):
+def cdf_cruncher(qs, batch_size, num_batches, n_iter, vectorisation, n_devices):
 
     bar = tqdm(position=0)
     pmngr = ProgressbarManager("", bar, vectorisation==VectorisationType.PMAP)
@@ -83,47 +78,51 @@ def cdf_cruncher(qs, batch_size, num_batches, n_iter, vectorisation):
         result = pedestrian(key)
         x = result["start"]
         lp = result["lp"]
-        return i+1, (jnp.searchsorted(qs, x), lp)
+        return i+1, (x, lp)
     
-    scan_f = vectorise_scan(step, None, 1, batch_size, 0, vectorisation, pmngr, lambda x: x)
+    scan_f = vectorise_scan(step, None, 1, batch_size, 0, vectorisation, n_devices, pmngr, lambda x: x)
 
-    def _count(i, _ix, _lps):
-        def step(s, data):
-            ix, lp = data
-            return jnp.logaddexp(s, jax.lax.select(ix <= i, lp, -jnp.inf)), None
-        return jax.lax.scan(step, jnp.array(-jnp.inf,float), (_ix, _lps))[0]
-    count = jax.vmap(_count, in_axes=(0,None,None), out_axes=0)
-    count_fn = vectorise(count, in_axes=(None,1,1), out_axes=0, batch_axis_size=batch_size, vectorisation=vectorisation, vmap_batch_size=0)
-
-
-    def _get_c(i):
+    def _get_c(i, key):
         pmngr.desc = f"{i}"
-        rng_keys = jax.random.split(jax.random.key(0), (num_batches,batch_size))
-        _, (ixs, lps) = scan_f(jnp.array(0,int), rng_keys)
+        rng_keys = jax.random.split(key, (num_batches,batch_size))
+        _, (xs, lps) = scan_f(jnp.array(0,int), rng_keys)
         
-        c = count_fn(jnp.arange(0,qs.size), ixs, lps)
-        c = jax.scipy.special.logsumexp(c, axis=0)
+        # result = jax.vmap(pedestrian)(jax.random.split(key, (num_batches,batch_size)))
+        # ixs = result["start"]
+        # lps = result["lp"]
+        
         s = jax.scipy.special.logsumexp(lps)
-        del ixs
+        
+        def _cdf(q):
+            lp_for_x_lt_q = jnp.where(xs < q, lps, jax.numpy.full_like(lps, -jnp.inf))
+            assert isinstance(lp_for_x_lt_q, jax.Array)
+            return jax.scipy.special.logsumexp(lp_for_x_lt_q)
+        
+        c = jax.lax.map(lambda a: _cdf(*a) , (qs,))
+        
+        del xs
         del lps
-    
-        return jax.lax.exp(c - s), s
+        
+        return c, s
+
     
     cs = []
     ss = []
+    key = jax.random.key(0)
     for i in tqdm(range(n_iter), position=1):
-        c, s = parallel_run(_get_c, (i+1,), batch_size, pconfig.vectorisation)
+        key, iter_key = jax.random.split(key)
+        c, s = parallel_run(_get_c, (i+1,iter_key),  batch_axis_size=batch_size, vectorisation=vectorisation, n_devices=n_devices)
         # print(f"{N=} {M=} {ixs.shape=}") # (N,M)
         cs.append(c)
         ss.append(s)
 
     cs = jnp.vstack(cs)
-    return jnp.mean(cs, axis=0)
-    # res_c = jax.scipy.special.logsumexp(cs, axis=0)
 
-    # ss = jnp.hstack(ss)
-    # res_s = jax.scipy.special.logsumexp(ss)
-    # return jax.lax.exp(res_c - res_s)
+    res_c = jax.scipy.special.logsumexp(cs, axis=0)
+
+    ss = jnp.hstack(ss)
+    res_s = jax.scipy.special.logsumexp(ss)
+    return jax.lax.exp(res_c - res_s)
     
 
 # n_sq batch_size num_batches n_iter
@@ -133,20 +132,22 @@ def cdf_cruncher(qs, batch_size, num_batches, n_iter, vectorisation):
 
 start_linspace = jnp.linspace(0., 3., args.n_qs)
 N_SAMPLES = args.num_batches * args.batch_size * args.n_iter
+# print(args)
 print(f"N_QS = {args.n_qs:_} N_SAMPLES = {N_SAMPLES:_}")
 
 if not args.old:
     pconfig = get_parallelisation_config(args)
-    gt_cdf = cdf_cruncher(start_linspace, batch_size=args.batch_size, num_batches=args.num_batches, n_iter=args.n_iter, vectorisation=pconfig.vectorisation)
+    gt_cdf = cdf_cruncher(start_linspace, batch_size=args.batch_size, num_batches=args.num_batches, n_iter=args.n_iter, vectorisation=pconfig.vectorisation, n_devices=pconfig.num_workers)
 else:
     assert args.num_batches == 1
     gt_cdf = cdf_cruncher_old(start_linspace, batch_size=args.batch_size, n_iter=args.n_iter)
 
 gt_pdf = jnp.hstack([jnp.array(0.),jnp.diff(gt_cdf)]) / (start_linspace[1] - start_linspace[0])
 
-jnp.save(f"evaluation/pedestrian/gt_xs-{args.n_qs}.npy", start_linspace)
-jnp.save(f"evaluation/pedestrian/gt_pdf_est-{args.n_qs}-{N_SAMPLES:_}.npy", gt_pdf)
-jnp.save(f"evaluation/pedestrian/gt_cdf-{args.n_qs}-{N_SAMPLES:_}.npy", gt_cdf)
+if not args.no_save:
+    jnp.save(f"evaluation/pedestrian/gt_xs-{args.n_qs}.npy", start_linspace)
+    jnp.save(f"evaluation/pedestrian/gt_pdf_est-{args.n_qs}-{N_SAMPLES:_}.npy", gt_pdf)
+    jnp.save(f"evaluation/pedestrian/gt_cdf-{args.n_qs}-{N_SAMPLES:_}.npy", gt_cdf)
 
 if args.show_plots:
     t0 = time()
@@ -158,9 +159,11 @@ if args.show_plots:
     t1 = time()
     print(f"Finished in {t1-t0:.3f}s")
     
+    plt.figure()
     plt.plot(start_linspace, gt_cdf)
     # plt.show()
     
+    plt.figure()
     weights = jax.lax.exp(lp - jax.scipy.special.logsumexp(lp))
     print(weights.sum())
     plt.hist(x, weights=weights, density=True, bins=100)
@@ -172,6 +175,7 @@ if args.show_plots:
     weights = weights[weights > 1e-9]
     print(x.shape)
 
+    plt.figure()
     kde = jax.scipy.stats.gaussian_kde(x, weights=weights)
     kde_pdf = kde(start_linspace)
     plt.plot(start_linspace, kde_pdf, color="tab:blue")
