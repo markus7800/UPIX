87dd028b60268412a2c4c57e14eaddb9d5549a00
markus7800 <markus.boeck98@gmail.com>
2025-08-30 08:19:07 +0200

    new experiments

--- experiments/pedestrian/scale/run.py
+++ experiments/pedestrian/scale/run.py
@@ -15,7 +15,8 @@ ndevices = int(args.ndevices)
 parallelisation = str(args.parallelisation)
 vectorisaton = str(args.vectorisaton)
 
-NCHAINS = [2**n for n in [3, 10, 13, 16, 17, 18, 19, 20] if n <= args.maxpow]
+NCHAINS2 = [2**n for n in [3, 10, 13, 16, 17, 18, 19, 20] if n <= args.maxpow]
+NCHAINS = [2**n for n in range(3,20) if 2**n not in NCHAINS2]
 print(NCHAINS)
 
 
