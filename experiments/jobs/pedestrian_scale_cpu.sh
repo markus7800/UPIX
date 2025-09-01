c14fb874059c0b9355116d6197c7b3f9f4fc4bea
Markus Böck <markus.boeck7800@hotmail.com>
2025-09-01 10:49:31 +0200

    update jobs

--- experiments/jobs/pedestrian_scale_cpu.sh
+++ experiments/jobs/pedestrian_scale_cpu.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 sbatch <<EOT
 #!/bin/bash
-#SBATCH --job-name=ped_cpu
+#SBATCH --job-name=ped_cpu_$1
 #SBATCH --partition=GPU-l40s
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=$1
 #SBATCH --cpu-freq=high
+#SBATCH --mail-user=markus.h.boeck@tuwien.ac.at
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+export UV_PROJECT_ENVIRONMENT=.venv-cpu
+uv sync --extra=cpu
 
 python3 experiments/runners/run_pedestrian_scale.py cpu $1 20 sequential pmap --no_progress
 
