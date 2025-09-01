b77ababcef7c64321903f2341bd362b5e7161b44
Markus Böck <markus.boeck7800@hotmail.com>
2025-09-01 17:16:58 +0200

    bump up gp_vi to 2^13

--- experiments/jobs/gp_vi_scale_cpu.py
+++ experiments/jobs/gp_vi_scale_cpu.py
@@ -4,7 +4,7 @@ import sys
 ndevices = int(sys.argv[1])
 
 jobstr = """
-python3 experiments/runners/run_gp_vi_scale.py cpu %d 12 sequential smap_local --no_progress
+python3 experiments/runners/run_gp_vi_scale.py cpu %d 13 sequential smap_local --no_progress
 """ % ndevices
 
 sbatch("CPU", "gp_vi_cpu", ndevices, jobstr)
--- experiments/jobs/gp_vi_scale_gpu.py
+++ experiments/jobs/gp_vi_scale_gpu.py
@@ -4,7 +4,7 @@ import sys
 ndevices = int(sys.argv[1])
 
 jobstr = """
-python3 experiments/runners/run_gp_vi_scale.py cuda %d 12 sequential smap_local --no_progress
+python3 experiments/runners/run_gp_vi_scale.py cuda %d 13 sequential smap_local --no_progress
 """ % ndevices
 
 sbatch("GPU", "gp_vi_gpu", ndevices, jobstr)
