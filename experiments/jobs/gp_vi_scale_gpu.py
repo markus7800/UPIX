2bad2b0dcd9cc456af7531d7ea3b1ee3ae0c7a5e
Markus Böck <markus.boeck7800@hotmail.com>
2025-09-01 15:08:52 +0200

    remove no_colors arg

--- experiments/jobs/gp_vi_scale_gpu.py
+++ experiments/jobs/gp_vi_scale_gpu.py
@@ -4,7 +4,7 @@ import sys
 ndevices = int(sys.argv[1])
 
 jobstr = """
-python3 experiments/runners/run_gp_vi_scale.py cuda %d 12 sequential smap_local --no_progress --no_colors
+python3 experiments/runners/run_gp_vi_scale.py cuda %d 12 sequential smap_local --no_progress
 """ % ndevices
 
 sbatch("GPU", "gp_vi_gpu", ndevices, jobstr)
--- experiments/jobs/pedestrian_scale_cpu.py
+++ experiments/jobs/pedestrian_scale_cpu.py
@@ -4,7 +4,7 @@ import sys
 ndevices = int(sys.argv[1])
 
 jobstr = """
-python3 experiments/runners/run_pedestrian_scale.py cpu %d 20 sequential pmap --no_progress --no_colors
+python3 experiments/runners/run_pedestrian_scale.py cpu %d 20 sequential pmap --no_progress
 """ % ndevices
 
 sbatch("CPU", "ped_cpu", ndevices, jobstr)
