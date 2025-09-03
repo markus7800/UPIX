4bac73145de85bda2426a0645d3b7ca223409d6e
Markus Böck <markus.boeck7800@hotmail.com>
2025-09-03 11:53:48 +0200

    rename results zip

--- experiments/jobs/zip_data.py
+++ experiments/jobs/zip_data.py
@@ -5,7 +5,7 @@ from uuid import uuid4
 now = datetime.now().strftime("%Y-%m-%d")
 uid = str(uuid4())[:8]
 shutil.make_archive(
-    f"experiment_data_{now}_{uid}",
+    f"results_data_{now}_{uid}",
     "zip",
     pathlib.Path("experiments", "data")
 )
