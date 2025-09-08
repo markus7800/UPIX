aca20ff9f15c826ee1dd52749e19ab28a89e198e
Markus Böck <markus.boeck7800@hotmail.com>
2025-09-08 19:13:33 +0200

    reduce omp threads for rjmcmc julia

--- experiments/runners/run_autogp_scale.py
+++ experiments/runners/run_autogp_scale.py
@@ -12,7 +12,7 @@ args = parser.parse_args()
 
 NPARTICLES = [2**n for n in range(args.minpow,args.maxpow+1)]
 
-CMD_TEMPLATE = "julia -t %d --project=evaluation/gp/autogp evaluation/gp/autogp/main.jl %d false"
+CMD_TEMPLATE = "OMP_NUM_THREADS=1 julia -t %d --project=evaluation/gp/autogp evaluation/gp/autogp/main.jl %d false"
 
 RUNNER_T0 = time.monotonic()
 
