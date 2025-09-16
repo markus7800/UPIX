e6895df616489974e8d20aefab524e637ff72824
Markus Böck <markus.boeck7800@hotmail.com>
2025-09-16 11:38:57 +0200

    update run_all

--- experiments/runners/run_all.sh
+++ experiments/runners/run_all.sh
@@ -7,11 +7,11 @@ uv sync --frozen --extra=$1
 
 python3 experiments/runners/run_pedestrian_scale.py $1 $2 0 20 sequential --no_progress 2>&1 | tee ped_$1_$2_$3.out
 
-python3 experiments/runners/run_gp_vi_scale.py $1 $2 0 13 sequential --no_progress 2>&1 | tee gp_vi_$1_$2_$3.out
+python3 experiments/runners/run_gp_vi_scale.py $1 $2 0 14 sequential --no_progress 2>&1 | tee gp_vi_$1_$2_$3.out
 
 python3 experiments/runners/run_gp_smc_scale.py $1 $2 0 15 sequential --no_progress 2>&1 | tee gp_smc_$1_$2_$3.out
 
-python3 experiments/runners/run_gmm_scale.py $1 $2 10 18 sequential --no_progress 2>&1 | tee gmm_$1_$2_$3.out
+python3 experiments/runners/run_gmm_scale.py $1 $2 0 18 sequential --no_progress 2>&1 | tee gmm_$1_$2_$3.out
 
 
 rm -rf .venv-$1_$2_$3
