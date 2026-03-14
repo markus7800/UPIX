f25c5e3d0bf02ec6676f65b7046289c719d4d7f0
markus7800 <markus.boeck7800@hotmail.com>
2026-03-14 14:59:32 +0100

    fix probs <0.0001

--- evaluation/urn/gendice.py
+++ evaluation/urn/gendice.py
@@ -72,13 +72,26 @@ s += draw_fn
 main = ""
 
 pois = [0.0024787508882582188, 0.014872513711452484, 0.04461752995848656, 0.08923499286174774, 0.1338524967432022, 0.1606229990720749, 0.16062292456626892, 0.13767682015895844, 0.10325776040554047, 0.06883830577135086, 0.041302964091300964, 0.02252892032265663, 0.011264455504715443, 0.00519897136837244, 0.002228131052106619, 0.0008912544581107795, 0.0003342198033351451, 0.0001179598766611889, 3.932005347451195e-05, 1.2416859135555569e-05, 3.72504700862919e-06, 1.064304456122045e-06, 2.9026290349065675e-07, 7.572104721020878e-08, 1.8930171208353386e-08, 4.5432755335639285e-09, 1.0484459966875193e-09, 2.3298787898973217e-10, 4.9925868994549205e-11, 1.0329426550215182e-11, 2.06589714953076e-12, 3.998528125357531e-13][:args.N+1]
-pois = [x / sum(pois) for x in pois]
-# main += "let N = discrete(" + ", ".join(map(str, pois)) + ") in\n"
+if any(p < 0.0001 for p in pois):
+    p1 = [p if p >= 0.0001 else 0.0 for p in pois]
+    n1 = sum(p1)
+    p1 = [p / n1 for p in p1]
+
+    p2 = [p if p < 0.0001 else 0.0 for p in pois]
+    n2 = sum(p2)
+    p2 = [p / n2 for p in p2]
+
+    main += "let N1 = discrete(" + ", ".join(map(str, p1)) + ") in\n"
+    main += "let N2 = discrete(" + ", ".join(map(str, p2)) + ") in\n"
+    main += f"let N = if flip {n1} then N1 else N2 in\n"
+else:
+    pois = [p / sum(pois) for p in pois]
+    main += "let N = discrete(" + ", ".join(map(str, pois)) + ") in\n"
 
 ps = [0.] * (2**bits)
 for i in range(args.N+1):
     ps[i] = 1/(args.N+1)
-main += "let N = discrete(" + ", ".join(map(str, ps)) + ") in\n"
+# main += "let N = discrete(" + ", ".join(map(str, ps)) + ") in\n"
 
 main += f"let tmp = observe(N > int({bits},0)) in\n"
 # main += "let xs = drawurn(N) in"
