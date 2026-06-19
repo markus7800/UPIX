b475403ad5ce8bc0c1c0bdcfb1ed857ece054058
Markus Böck <markus.boeck98@gmail.com>
2026-06-19 19:41:17 -0400

    add tmpdir

--- experiments/runners/run_ped_gt.sh
+++ experiments/runners/run_ped_gt.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+
+export TMPDIR=~/tmp
+
 for seed in {0..4}; do
     uv run --python python3.13 --extra=cuda --frozen evaluation/pedestrian/ground_truth.py sequential vmap_global 100 1_000_000 1_000 1_000 $seed
 done
