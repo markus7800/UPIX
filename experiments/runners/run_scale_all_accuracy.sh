0ed4387c7b106f641aa1a5d2654fbb8444a3026b
Markus Böck <markus.boeck98@gmail.com>
2026-06-23 19:15:05 -0400

    add ncpu arg

--- experiments/runners/run_scale_all_accuracy.sh
+++ experiments/runners/run_scale_all_accuracy.sh
@@ -1,26 +1,25 @@
 #! /bin/bash
+# npcu
 
 export TMPDIR=~/tmp
 export OMP_NUM_THREADS=1
 
 # l40s 150min
-uv run --frozen -p python3.13 --extra=cuda evaluation/pedestrian/viz_mcmc_scale.py sequential vmap_global -vmap_batch_size 524288 -maxpow ${1:-20} --no_progress
+uv run --frozen -p python3.13 --extra=cuda evaluation/pedestrian/viz_mcmc_scale.py sequential vmap_global -vmap_batch_size 524288 -maxpow ${2:-20} --no_progress
 
 # l40s 37min
-uv run --frozen -p python3.13 --extra=cuda --with=pandas evaluation/gp/viz_vi_elbo_scale.py sequential vmap_global -maxpow ${2:-14} --no_progress
+uv run --frozen -p python3.13 --extra=cuda --with=pandas evaluation/gp/viz_vi_elbo_scale.py sequential vmap_global -maxpow ${3:-14} --no_progress
 
 # l40s ~20min
-uv run --frozen -p python3.13 --extra=cuda --with=pandas evaluation/gp/viz_smc_particle_scale.py sequential vmap_local -maxpow ${3:-15} --no_progress
+uv run --frozen -p python3.13 --extra=cuda --with=pandas evaluation/gp/viz_smc_particle_scale.py sequential vmap_local -maxpow ${4:-15} --no_progress
 
 # l40s 392min, but corrupted results somehow
-# uv run --frozen -p python3.13 --extra=cuda evaluation/gmm/viz_mcmc_scale.py sequential vmap_global -vmap_batch_size 524288 -maxpow ${4:-18} --no_progress
+# uv run --frozen -p python3.13 --extra=cuda evaluation/gmm/viz_mcmc_scale.py sequential vmap_global -vmap_batch_size 524288 -maxpow ${5:-18} --no_progress
 
-# uv run --frozen -p python3.13 --extra=cpu --with=pandas evaluation/gp/viz_vi_elbo_scale_prelim.py sequential smap_local -host_device_count 8 --no_progress --cpu
+# uv run --frozen -p python3.13 --extra=cpu evaluation/pedestrian/viz_mcmc_scale.py sequential pmap -vmap_batch_size 524288 -maxpow ${2:-20} -host_device_count $1 --no_progress --cpu
 
-# uv run --frozen -p python3.13 --extra=cpu evaluation/pedestrian/viz_mcmc_scale.py sequential pmap -vmap_batch_size 524288 -maxpow ${1:-20} -host_device_count 8 --no_progress --cpu
+# uv run --frozen -p python3.13 --extra=cpu --with=pandas evaluation/gp/viz_vi_elbo_scale.py sequential smap_local -maxpow ${3:-14} -host_device_count $1 --no_progress --cpu
 
-# uv run --frozen -p python3.13 --extra=cpu --with=pandas evaluation/gp/viz_vi_elbo_scale.py sequential smap_local -maxpow ${2:-14} -host_device_count 8 --no_progress --cpu
+# uv run --frozen -p python3.13 --extra=cpu --with=pandas evaluation/gp/viz_smc_particle_scale.py sequential smap_local -maxpow ${4:-15} -host_device_count $1 --no_progress --cpu
 
-# uv run --frozen -p python3.13 --extra=cpu --with=pandas evaluation/gp/viz_smc_particle_scale.py sequential smap_local -maxpow ${3:-15} -host_device_count 8 --no_progress --cpu
-
-uv run --frozen -p python3.13 --extra=cpu evaluation/gmm/viz_mcmc_scale.py sequential pmap -vmap_batch_size 524288 -maxpow ${4:-18} -host_device_count 8 --no_progress --cpu
+uv run --frozen -p python3.13 --extra=cpu evaluation/gmm/viz_mcmc_scale.py sequential pmap -vmap_batch_size 524288 -maxpow ${5:-18} -host_device_count $1 --no_progress --cpu
--- experiments/runners/run_scale_all_experiments.sh
+++ experiments/runners/run_scale_all_experiments.sh
@@ -4,4 +4,4 @@
 time bash experiments/runners/run_scale_all_references.sh $1 cpu $3 $4 $5 $6
 time bash experiments/runners/run_scale_all_upix.sh cpu $1 cpu $3 $4 $5 $6
 time bash experiments/runners/run_scale_all_upix.sh cuda $2 gpu $3 $4 $5 $6
-time bash experiments/runners/run_scale_all_accuracy.sh $3 $4 $5 $6
+time bash experiments/runners/run_scale_all_accuracy.sh $1 $3 $4 $5 $6
